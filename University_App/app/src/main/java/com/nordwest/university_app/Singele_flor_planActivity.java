6c9b6d778e794b28e6666e5333a1db9d6a572557
NordWest01 <mihairmcr7@gmail.com>
2018-12-05 21:19:04 +0000

    WebDashboard, single_image_activity and also the SliderActivity were commented

--- University_App/app/src/main/java/com/nordwest/university_app/Singele_flor_planActivity.java
+++ University_App/app/src/main/java/com/nordwest/university_app/Singele_flor_planActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Singele_flor_planActivity extends AppCompatActivity {
-
+    //variable declaration
     TextView closeReturn;
     ImageView floorPlanImage;
     public static String FLOOR_ID = "FLOOR_ID";
@@ -20,13 +20,18 @@ public class Singele_flor_planActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //set corresponding layout
         setContentView(R.layout.activity_singele_flor_plan);
+
+        //get the the extra data sent when the activity was launched
         Bundle bundle = getIntent().getExtras();
+        //save in string format the bundled data gotten prom previous activity
         floorID = bundle.getString(FLOOR_ID).trim();
-        Toast.makeText(this, floorID.substring(0,2), Toast.LENGTH_SHORT).show();
 
+        //links the java object with the XML widget
         floorPlanImage = findViewById(R.id.singelFloorPlanImage);
-
+        /*checks the first two letters of room sent through the main threat when the activity was launched. Compare with the known rooms on the floor plans and
+        therefore, launch the corresponding floor plan according to the room where the user has classes, which was sent through the putExtra method*/
         if (floorID.substring(0,2).equals("RL")){
             floorPlanImage.setImageResource(R.drawable.level3);
         }else if(floorID.substring(0,2).equals("RO")){
@@ -37,16 +42,18 @@ public class Singele_flor_planActivity extends AppCompatActivity {
             floorPlanImage.setImageResource(R.drawable.ground);
 
         }
+
         //makes the statue bar background transparent
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-
+        //links the java object with the widget on the layout
         closeReturn = findViewById(R.id.closeReturn);
 
-
+        //set clickListener for close button on the top of the activity
         closeReturn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //send the threat to the previous activity
                 Singele_flor_planActivity.super.onBackPressed();
 
             }
--- University_App/app/src/main/java/com/nordwest/university_app/SliderActivity.java
+++ University_App/app/src/main/java/com/nordwest/university_app/SliderActivity.java
@@ -9,26 +9,32 @@ import android.view.WindowManager;
 import android.widget.TextView;
 
 public class SliderActivity extends AppCompatActivity {
+    // variable declaration
     TextView closeReturn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //set the corresponding layout on onCreate time
         setContentView(R.layout.activity_slider);
-    //instance of out viewPager object and link to the widget on the layout
+        //instance of out viewPager object and link to the widget on the layout
         ViewPager viewPager = findViewById(R.id.viewPager);
+        //initiates new ImageAdapter object
         ImageAdapter adapter = new ImageAdapter(this);
+        //set to the ViewPager the ImageAdaptor to display all images
         viewPager.setAdapter(adapter);
+
         //makes the statue bar background transparent
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-
+        //links the java object to the layout widget
         closeReturn = findViewById(R.id.closeReturnToTimetable);
 
-
+        //set clickListener for close button on the top of the activity
         closeReturn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //send the threat to the previous activity
                 SliderActivity.super.onBackPressed();
 
             }
